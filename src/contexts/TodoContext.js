c7f830e646d83c361768cc6ce9bce34abff86431
r1-yash <yashsinghalxd@gmail.com>
2025-12-01 22:44:53 +0530

    Initial commit

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,9 @@
+import {createContext, useConext, useContext} from "react"
+
+export const TodoContext = createContext({})
+
+export const TodoProvider = TodoContext.Provider
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
