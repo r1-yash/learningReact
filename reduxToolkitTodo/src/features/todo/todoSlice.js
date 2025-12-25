d26638517f8184af4b3c04fff46c2c76607be69f
r1-yash <yashsinghalxd@gmail.com>
2025-12-25 17:58:26 +0530

    Functionality of reducers

---
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,37 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit'
+//nanoid generates unique id
+
+const initialState = {
+    todos: [{id:1, text:"Hello world"}]
+}
+//just an object
+
+
+export const todoSlice = createSlice({
+    name: 'todo', //redux syntax. will be used like todo/addTodo
+    initialState,
+    reducers: { //reduces define how a state will change, it ONLY does the work action just describes what to be done
+        addTodo: (state, action) =>{
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        }, 
+        removeTodo: (state,action) =>{
+            state.todos = state.todos.filter((todo)=> todo.id !== action.payload)
+        },
+        updateTodo: (state,action) =>{
+
+            const {id, text} = action.payload; //the id and text will come from this, otherwise js will throw reference error
+            const todo = state.todos.find((todo) => todo.id === id)
+            if(todo){
+                todo.text = text;
+            }
+        }
+    }
+})
+
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer
