b1d427fbe4f6db4cd08eb4ef043df3346a43d191
r1-yash <yashsinghalxd@gmail.com>
2025-12-29 00:32:36 +0530

    worked with display and no toggle

--- selfTaskBoard/src/components/BoardItems.jsx
+++ selfTaskBoard/src/components/BoardItems.jsx
@@ -7,9 +7,45 @@ function BoardItems (){
 
     //for edit functionality, we need two states .. one for togglign state of edit and for for holding the temporary text to be put
     const [text, setText] = useState(task.text)
-    const [edit, isTaskEditable] = useState(false)
+    const [isEdit, setIsEditing] = useState(false)
 
-    
+    return (
+        <div>
+        style={{
+                border: "1px solid #ccc",
+                padding: "10px",
+                marginBottom: "8px",
+                borderRadius: "6px",
+            }}
+
+            {/* - readOnly when not editing
+            - editable when isEditing = true   */}
+        <input
+                type="text"
+                value={text}
+                readOnly={!isEdit}
+                onChange={(e) => setText(e.target.value)}
+                style={{
+                border: isEdit ? "1px solid black" : "none",
+                outline: "none",
+                width: "60%",
+                }}
+        />
+
+        <button
+        onClick={() => {
+          if (isEdit) {
+            editTask(task.id, text); // updates global state
+          }
+          setIsEditing(!isEdit);
+        }}
+        style={{ marginLeft: "6px" }}
+      >
+        {isEdit ? "Save" : "Edit"}
+      </button>        
+
+        </div>
+    )
 
 
 
