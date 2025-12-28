8e1b7cdbc3672ba3ea4fe789ead5c46eed4b9b1a
r1-yash <yashsinghalxd@gmail.com>
2025-12-28 17:24:11 +0530

    Stage 1 for context added, index file

--- selfTaskBoard/src/contetxs/BoardContext.js
+++ selfTaskBoard/src/contetxs/BoardContext.js
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 
 export const BoardContext = createContext ({
-    task: [],
+    tasks: [],
     addTask: () => {},
     editTask: () => {},
     removeTask: () =>{},
---
+++ selfTaskBoard/src/contetxs/index.js
@@ -0,0 +1,1 @@
+import { BoardContext, BoardProvider, useBoard } from "./BoardContext";
